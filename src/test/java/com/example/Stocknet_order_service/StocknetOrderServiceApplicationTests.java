3e4df1b862d6d9406fa7ccf2c13b0ba001bec75e
BryamCode <bryamfernandezuriarte22@gmail.com>
2026-02-06 17:20:33 -0500

    Creacion del servicio de ordenes

---
+++ src/test/java/com/example/Stocknet_order_service/StocknetOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Stocknet_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocknetOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
